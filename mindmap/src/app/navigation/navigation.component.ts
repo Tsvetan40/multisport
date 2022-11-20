f242a680a9c4cfa94b4310680c1a62bfd629207b
Tsvetan40 <tsvetan.gabrovski@abv.bg>
2022-11-20 14:43:20 +0200

    master initial commit, responsive navigation

---
+++ mindmap/src/app/navigation/navigation.component.ts
@@ -0,0 +1,46 @@
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+
+export class NavigationComponent implements OnInit{
+  public screenWidth!: number;
+  public isClicked!: boolean;
+  public show!: boolean;
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+   
+    if (this.screenWidth > 400) {
+      this.isClicked = this.isClicked = true;
+      return;
+    }
+    
+    this.isClicked = this.isClicked = false;
+  }
+
+  chacgeClicked(): void {
+    this.isClicked = !this.isClicked;
+    this.show = !this.show;
+  }
+
+    @HostListener('window:resize')
+    onResize(): void {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth > 400) {
+      this.isClicked = false;
+      this.show = true;
+      return;
+    }
+
+    if (this.isClicked) {
+      this.show = true;
+      return;
+    }
+
+    this.show = false;
+  }
+}
