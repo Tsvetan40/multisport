6460d580327ecca7f7b2f751660147eb80bc0c82
Tsvetan40 <tsvetan.gabrovski@abv.bg>
2023-02-02 14:35:08 +0200

    navigation to specific article

--- angular-multisport/src/app/public/articles/public-articles.component.ts
+++ angular-multisport/src/app/public/articles/public-articles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from 'src/app/models/page/Article';
 import { PublicService } from 'src/app/services/public.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-public-articles',
@@ -8,15 +9,21 @@ import { PublicService } from 'src/app/services/public.service';
   styleUrls: ['./public-articles.component.css']
 })
 export class PublicArticlesComponent implements OnInit{
-
+  private baseUrl: string
   allArticles: Article[] 
-  article: Article
 
-  constructor(private publicService: PublicService) {
+  constructor(private publicService: PublicService, private router: Router) {
     this.allArticles = []
-    this.article = new Article()
+    this.baseUrl = '/multisport/articles'
    }
-  
+
+   goTo(event: Event) {
+      const p = event.target as HTMLParagraphElement;
+      const title =  p.textContent
+
+      this.router.navigateByUrl(`${this.baseUrl}/${title}`)  
+   }
+
    ngOnInit(): void {
     this.publicService.getAllArticle().subscribe(
       (data) => {
