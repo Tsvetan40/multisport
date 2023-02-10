95040cb04e6f5848ff4c2f0d4f592c94fc66ef4a
Tsvetan40 <tsvetan.gabrovski@abv.bg>
2023-02-10 19:18:04 +0200

    master handle response for newcenter

--- angular-multisport/src/app/admin/centers/centers.component.ts
+++ angular-multisport/src/app/admin/centers/centers.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RelaxCenter } from 'src/app/models/centers/RelaxCenter';
 import { SportCenter } from 'src/app/models/centers/SportCenter';
 import { CenterType } from 'src/app/models/centers/typecenter/CenterType';
@@ -19,7 +20,7 @@ export class CentersComponent {
   pictures: string[] = []
   @ViewChild('centerForm') centerForm!: NgForm
 
-  constructor (private adminService: AdminService) {} 
+  constructor (private adminService: AdminService, private router: Router) {} 
 
   addPictureList(picture: HTMLInputElement) {
     if (!picture.value) {
@@ -49,7 +50,19 @@ export class CentersComponent {
                               'centerType': 'RELAX_CENTER',
                               'services': this.services
                             } 
-      this.adminService.saveSportCenter(sportCenter).subscribe()
+      this.adminService.saveSportCenter(sportCenter).subscribe(
+        data => {
+          alert("Center Saved!")
+        },
+        error => {
+          if (error['status'] == 403) {
+            this.router.navigate(["/multisport"])
+          } else {
+            alert("Center Not Saved")
+          }
+        }
+
+      )
     } else {
       const relaxCenter = {
                             'name': this.name, 
@@ -59,7 +72,18 @@ export class CentersComponent {
                             'centerType': 'RELAX_CENTER',
                             'services': this.getServices()
                           }
-        this.adminService.saveRelaxCenter(relaxCenter).subscribe()
+        this.adminService.saveRelaxCenter(relaxCenter).subscribe(
+          data => {
+            alert("Center Saved!")
+          },
+          error => {
+            if (error['status'] == 403) {
+              this.router.navigate(["/multisport"])
+            } else {
+              alert("Center Not Saved")
+            }
+          }
+        )
     }
   
     this.address = ''
