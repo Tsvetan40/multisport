f560958382883bb7cf4da4bf4f76c59c71d26f4e
Tsvetan40 <tsvetan.gabrovski@abv.bg>
2023-03-05 11:29:51 +0200

    security-request-matchers cors configuration and unauthenticated user requests in front-end app

--- spring-multisport/src/main/java/com/demo/multisport/security/SecurityConfig.java
+++ spring-multisport/src/main/java/com/demo/multisport/security/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -14,20 +19,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                    .mvcMatchers(HttpMethod.POST, "multisport")
+                    .permitAll()
                 .and()
                 .authorizeHttpRequests()
                     .mvcMatchers("multisport/admin/**")
                     .hasAuthority("ADMIN")
                 .and()
                 .authorizeHttpRequests()
-                    .mvcMatchers(HttpMethod.POST, "multisport/**")
+                    .mvcMatchers("multisport/login", "multisport/register")
                     .authenticated()
                 .and()
                 .authorizeHttpRequests()
-                    .mvcMatchers("multisport/login", "multisport/register")
+                    .mvcMatchers(HttpMethod.POST, "multisport/**")
                     .authenticated()
                 .and()
                 .authorizeHttpRequests()
@@ -38,6 +49,20 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOrigins(List.of("http://localhost:4200"));
+        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        cors.setAllowedHeaders(List.of(CorsConfiguration.ALL));
+        cors.setAllowCredentials(true);
+        cors.setExposedHeaders(List.of("Origin", "Content-TypeCenter", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        UrlBasedCorsConfigurationSource url = new UrlBasedCorsConfigurationSource();
+        url.registerCorsConfiguration("/**", cors);
+        return url;
     }
 
     @Bean
